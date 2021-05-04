6caa4e7da5a98cf6614a9b253cbd3cf1e4c1e04e
Ahmad Abdulazeez Hashim <79423908+lazoo97@users.noreply.github.com>
2021-05-04 14:45:57 +0400

    create express server

--- CW-2/backend/app.js
+++ CW-2/backend/app.js
@@ -1,1 +1,12 @@
 /* Create your Express Server Here */
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+app.use(function(request, response){
+    console.log("In comes a request to: " + request.url);
+    response.end("Server working!");
+})
+
+http.createServer(app).listen(3000);
