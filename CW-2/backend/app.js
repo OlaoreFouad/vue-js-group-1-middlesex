cbba79276fc1b590d04dea0b02da1e490bba140d
unknown <durotimijohnson@gmail.com>
2021-05-05 12:13:07 +0400

    Implemented the /lesson route

--- CW-2/backend/app.js
+++ CW-2/backend/app.js
@@ -7,6 +7,13 @@ var user= [
     {'email': 'user@email.com', 'password': 'mypassword'}
 ];
 
+var lessonList = [
+    {'topic': 'math', 'location': 'hendon', 'price': 100},
+    {'topic': 'math', 'location': 'UK', 'price': 90},
+    {'topic': 'math', 'location': 'US', 'price': 80},
+    {'topic': 'math', 'location': 'Nigeria', 'price': 70},
+];
+
 app.use(function(request, response, next){
     console.log("In comes a request to: " + request.url);
     next();
@@ -16,6 +23,12 @@ app.get('/user', function(req, res){
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(JSON.stringify(user));
+});
+
+app.get('/lesson', function(req, res){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(JSON.stringify(lessonList));
 });
 
 http.createServer(app).listen(3000);
